334ce692ca70d727be645a71092dfe5f7cac8ad1
shahbazdo5555-ux <shahbazdo5555@gmail.com>
2026-03-14 10:06:22 -0700

    Change fetch URL to CORS proxy for data retrieval
    Updated fetch URL to use a CORS proxy for product data.

--- script.js
+++ script.js
@@ -27,8 +27,8 @@
       async function fetchData() {
         loader.style.display = 'flex';
         try {
-// YEH BHI BEST HAI
-const res = await fetch('https://crossorigin.me/https://fakestoreapi.com/products');
+// YEH USE KARO - BILKUL KAAM KAREGA
+const res = await fetch('https://corsproxy.io/?https://fakestoreapi.com/products');
           const data = await res.json();
           allProducts = data;
           filteredProducts = [...allProducts];
